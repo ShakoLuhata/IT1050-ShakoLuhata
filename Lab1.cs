3dfcc752f3436a18d9987d494ea1dd7f68764447
ShakoLuhata <59976903+ShakoLuhata@users.noreply.github.com>
2020-04-03 14:05:12 -0700

    Add files via upload

---
+++ Lab1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worl From Shako Luhata");
+        }
+    }
+}
